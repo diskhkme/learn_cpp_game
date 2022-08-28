f54b865770939a9d7899b9f0e49773c6ddbaedfb
Hyungki Kim <diskhkme@gmail.com>
2022-08-29 00:06:12 +0900

    turning back to the plane raylib

--- 0_Introduction/Main.cpp
+++ 0_Introduction/Main.cpp
@@ -1,4 +1,4 @@
-#include "raylib-cpp.hpp" // 화면을 그리는 데 필요한 기능들이 구현되어 있는 헤더 파일
+#include "raylib.h" // 화면을 그리는 데 필요한 기능들이 구현되어 있는 헤더 파일
 
 // 화면에 그릴 수 있는 여러가지는 https://www.raylib.com/cheatsheet/cheatsheet.html 사이트를 참고하세요
 // 빌드 결과 .exe 파일은 "CPP_Practice_Game/bin/Debug 또는 Release 폴더 안에 들어가게 되어 있습니다.
@@ -9,12 +9,12 @@ int main() {
 	int screenHeight = 450;
 
 	// 윈도우 만들기
-	raylib::Window window(screenWidth, screenHeight, "0_Introduction"); // Parameter: ((int)가로, (int)세로, (std::string)창 이름)
+	InitWindow(screenWidth, screenHeight, "0_Introduction"); // Parameter: ((int)가로, (int)세로, (std::string)창 이름)
 
 	SetTargetFPS(60); // 화면을 1초에 몇번 그릴 것인지 (보통은 60번을 사용함. 60 fps(frames per second)
 
 	// 창을 닫을 때까지 while 반복문을 돌면서 화면을 그리기 위한 함수들을 호출
-	while (!window.ShouldClose())
+	while (!WindowShouldClose())
 	{
 		BeginDrawing(); // 무언가를 그리기 전에 BeginDrawing()이 먼저 호출되어야 합니다.
 
@@ -22,7 +22,7 @@ int main() {
 		Color backgroundColor = Color{ 50,50,50,255 };	// Color는 색상 정보를 담기 위한 구조체입니다. R,G,B,A 값을 설정할 수 있습니다.
 														// 예를들어 빨간색은 Color{255,0,0,255}로 정의됩니다.
 		
-		window.ClearBackground(backgroundColor); // 배경을 위에 설정한 backgroundColor값으로 칠합니다.
+		ClearBackground(backgroundColor); // 배경을 위에 설정한 backgroundColor값으로 칠합니다.
 
 		Color rectangleColor = Color{ 255, 255, 255, 255 }; // 사각형의 색상인 흰색을 rectangleColor에 저장합니다.
 
--- 1_Variable/Main.cpp
+++ 1_Variable/Main.cpp
@@ -1,4 +1,4 @@
-#include "raylib-cpp.hpp" 
+#include "raylib.h" 
 
 // 화면에 그릴 수 있는 여러가지는 https://www.raylib.com/cheatsheet/cheatsheet.html 사이트를 참고하세요
 // 빌드 결과 .exe 파일은 "CPP_Practice_Game/bin/Debug 또는 Release 폴더 안에 들어가게 되어 있습니다.
@@ -7,7 +7,7 @@ int main() {
 	int screenWidth = 800;
 	int screenHeight = 450;
 
-	raylib::Window window(screenWidth, screenHeight, "0_Introduction");
+	InitWindow(screenWidth, screenHeight, "1_Variable");
 
 	SetTargetFPS(60); 
 
@@ -22,7 +22,7 @@ int main() {
 	const Color backgroundColor = Color{ 224, 251, 252, 255 };
 	const Color rectangleColor = Color{ 238, 108, 77, 255 };
 		
-	while (!window.ShouldClose())
+	while (!WindowShouldClose())
 	{
 		// 일반적으로 게임을 만들때 loop 안에서는 데이터 조작과 조작된 데이터를 기반으로 한 화면 그리기를 수행합니다.
 		// 여기에서도 이렇게 두 부분으로 구분해서 구현할 것이며, 중괄호, 들여쓰기 등은 문법적으로 의미는 없고, 보기 좋게 하기위해 들어간 것입니다.
@@ -35,7 +35,7 @@ int main() {
 		{ // 그리기 (Draw)
 			BeginDrawing(); 
 
-				window.ClearBackground(backgroundColor); // 배경색은 whiel문 밖에 선언되어 있기 때문에, 여기서 접근할 수 있습니다.
+				ClearBackground(backgroundColor); // 배경색은 whiel문 밖에 선언되어 있기 때문에, 여기서 접근할 수 있습니다.
 		
 				DrawRectangle(rectPositionX, rectPositionY, rectWidth, rectHeight, rectangleColor);	
 				// 사각형 그리기에 상수값(10,20)을 사용하는 대신, 위에 선언해둔 변수를 사용합니다.
