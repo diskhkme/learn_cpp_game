292b02151c5de39c147f610a0fc8bdff9b117889
Hyungki Kim <diskhkme@gmail.com>
2022-09-01 22:23:53 +0900

    function, draw enemy

--- raylib_cpp_game/Main.cpp
+++ raylib_cpp_game/Main.cpp
@@ -9,11 +9,23 @@ void Update(int& playerPositionX, int& playerPositionY)
 {
 	// IsKeyDown(keyCode) 함수를 사용하면, 해당 키보드가 눌렸는지 여부를 bool로 반환해줍니다.
 
-	if (IsKeyDown(KEY_RIGHT)) playerPositionX += 2;
-	else if (IsKeyDown(KEY_LEFT)) playerPositionX -= 2;
+	if (IsKeyDown(KEY_D)) playerPositionX += 2;
+	else if (IsKeyDown(KEY_A)) playerPositionX -= 2;
 
-	if (IsKeyDown(KEY_UP)) playerPositionY -= 2;
-	else if (IsKeyDown(KEY_DOWN)) playerPositionY += 2;
+	if (IsKeyDown(KEY_W)) playerPositionY -= 2;
+	else if (IsKeyDown(KEY_S)) playerPositionY += 2;
+}
+
+// 적을 그리는 연산은 이 곳에 함수로 모아 뒀습니다.
+void DrawEnemy(int enemyPositionX[], int enemyPositionY[], int enemyCount, int enemyRadius, Color enemyColor)
+{
+	for (int i = 0; i < enemyCount; i++)
+	{
+		DrawCircle(enemyPositionX[i], enemyPositionY[i], enemyRadius, enemyColor); // 적 그리기. 적은 동그라미로 그려줍시다.
+		DrawCircleLines(enemyPositionX[i], enemyPositionY[i], enemyRadius, BLACK); // 똑같은 위치에 속이 빈 원을 검은 테두리로 덧그리면 눈에 더 잘뜨입니다.
+
+		// (BLACK, RED, GREEN, BLUE, WHITE와 같은 대표적인 색상은 매크로로 정의되어 있어 편하게 사용할 수 있습니다.)
+	}
 }
 
 int main() {
@@ -32,6 +44,20 @@ int main() {
 	const int playerWidth = 20; // 플레이어 사각형의 크기는 변하지 않는다고 합시다.
 	const int playerHeight = 20;
 	const Color playerColor = Color{ 238, 108, 77, 255 };
+
+	//--- 화면에 그릴 적 관련 변수
+	const int enemyCount = 10; // 적은 10 마리를 그릴 예정입니다. 배열 크기로 사용하기 위해 const 여야 한다는 것을 잊지 마세요
+	int enemyPositionX[enemyCount]; 
+	int enemyPositionY[enemyCount]; // 적의 X위치와 Y위치를 저장할 배열을 만들어 줍니다.
+	const int enemyRadius = 10; // 적은 동그라미로 그릴 것이라 width/height 대신 반지름이 필요합니다.
+	const Color enemyColor = Color{ 200, 150, 255, 255 };
+
+	// 적의 X, Y 위치 배열에 값을 넣어주겠습니다.
+	for (int i = 0; i < enemyCount; i++)
+	{
+		enemyPositionX[i] = screenWidth - 20; // 적의 X 위치는 항상 화면 오른쪽에서 20만큼 떨어진 지점에 만듭니다.
+		enemyPositionY[i] = GetRandomValue(0, screenHeight); // Y 위치는 무작위로 만들어 봅시다. GetRandomValue() 함수로 0과 screenHeight 사이의 무작위 수를 생성할 수 있습니다. (라이브러리 기능)
+	}
 
 	//--- 배경색과 사각형의 색상을 바꾸지 않는다고 하면, 상수로 선언
 	const Color backgroundColor = Color{ 224, 251, 252, 255 };
@@ -56,6 +82,8 @@ int main() {
 			ClearBackground(backgroundColor); // 배경 그리기
 
 			DrawRectangle(playerPositionX, playerPositionY, playerWidth, playerHeight, playerColor); // 플레이어 그리기
+
+			DrawEnemy(enemyPositionX, enemyPositionY, enemyCount, enemyRadius, enemyColor); // 적 그리기
 
 			EndDrawing();
 		}
