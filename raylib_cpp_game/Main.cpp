08e8d6da10a368262b13c71183d6ae857a855020
Hyungki Kim <diskhkme@gmail.com>
2022-09-01 21:00:24 +0900

    variable

--- raylib_cpp_game/Main.cpp
+++ raylib_cpp_game/Main.cpp
@@ -9,31 +9,41 @@ int main() {
 	int screenHeight = 450;
 
 	// 윈도우 만들기
-	InitWindow(screenWidth, screenHeight, "0_Introduction"); // Parameter: ((int)가로, (int)세로, (std::string)창 이름)
+	InitWindow(screenWidth, screenHeight, "1_Variable"); // Parameter: ((int)가로, (int)세로, (std::string)창 이름)
 
 	SetTargetFPS(60); // 화면을 1초에 몇번 그릴 것인지 (보통은 60번을 사용함. 60 fps(frames per second)
+
+	//--- 화면에 그릴 사각형 관련 변수
+	int rectPositionX = 10;
+	int rectPositionY = 10;
+	int rectWidth = 20;
+	int rectHeight = 20;
+
+	//--- 배경색과 사각형의 색상을 바꾸지 않는다고 하면, 상수로 선언
+	const Color backgroundColor = Color{ 224, 251, 252, 255 };
+	const Color rectangleColor = Color{ 238, 108, 77, 255 };
 
 	// 창을 닫을 때까지 while 반복문을 돌면서 화면을 그리기 위한 함수들을 호출
 	while (!WindowShouldClose())
 	{
-		BeginDrawing(); // 무언가를 그리기 전에 BeginDrawing()이 먼저 호출되어야 합니다.
+		// 일반적으로 게임을 만들때 loop 안에서는 데이터 조작과 조작된 데이터를 기반으로 한 화면 그리기를 수행합니다.
+		// 여기에서도 이렇게 두 부분으로 구분해서 구현할 것이며, 중괄호, 들여쓰기 등은 문법적으로 의미는 없고, 보기 좋게 하기위해 들어간 것입니다.
 
-		//------ 그리기 ---------------------------------------------------//
-		Color backgroundColor = Color{ 50,50,50,255 };	// Color는 색상 정보를 담기 위한 구조체입니다. R,G,B,A 값을 설정할 수 있습니다.
-														// 예를들어 빨간색은 Color{255,0,0,255}로 정의됩니다.
+		{ // 데이터 조작 (Update)
+			rectPositionX = rectPositionX + 1; // 매 loop마다 rectPosition 변수를 1씩 증가시킵니다.
 
-		ClearBackground(backgroundColor); // 배경을 위에 설정한 backgroundColor값으로 칠합니다.
+		}
 
-		Color rectangleColor = Color{ 255, 255, 255, 255 }; // 사각형의 색상인 흰색을 rectangleColor에 저장합니다.
+		{ // 그리기 (Draw)
+			BeginDrawing();
 
-		DrawRectangle(10, 10, 20, 20, rectangleColor);	// DrawRectangle 함수를 호출해 배경 위에 사각형을 그립니다.
-														// Parameter: ((int)가로위치, (int)세로위치, (int)사각형의 넓이, (int)사각형의 높이, (Color)사각형 색상)
-														// (Note) 사각형의 위치와 크기를 원하는대로 바꿔 보세요
+			ClearBackground(backgroundColor); // 배경색은 whiel문 밖에 선언되어 있기 때문에, 여기서 접근할 수 있습니다.
 
-		DrawText("Hello World!", 190, 200, 20, LIGHTGRAY); // DrawText를 사용해 화면에 문자를 표시할 수도 있습니다!
-		//-----------------------------------------------------------------//
+			DrawRectangle(rectPositionX, rectPositionY, rectWidth, rectHeight, rectangleColor);
+			// 사각형 그리기에 상수값(10,20)을 사용하는 대신, 위에 선언해둔 변수를 사용합니다.
 
-		EndDrawing(); // 그리기가 끝나면, EndDrawing()을 호출해야 합니다.
+			EndDrawing();
+		}
 	}
 
 	return 0;
@@ -41,6 +51,6 @@ int main() {
 
 //--- Practice
 
-// 1. 화면의 크기를 더 크게, 또는 더 작게 만들어 보세요.
-// 2. 사각형을 화면의 다른 위치에, 다른 크기로 만들어 보세요.
-// 3. 사각형 대신 원을 화면에 그려보세요. 
+// 1. 사각형의 이동 속도를 더 증가시켜보세요.
+// 2. 사각형의 이동 방향을 바꿔 보세요. 예를들어 오른쪽 아래로 이동하도록 해 보세요.
+// 3. 사각형이 화면 끝까지 가면 사라져버립니다. 이럴 경우 화면 왼쪽에서 다시 나타나도록 코드를 수정해 보세요.
