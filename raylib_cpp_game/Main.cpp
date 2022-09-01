1fe490b496fe11815b0e28433d081279008656f9
Hyungki Kim <diskhkme@gmail.com>
2022-09-02 01:05:30 +0900

    pointer and ref, moving enemy

--- raylib_cpp_game/Main.cpp
+++ raylib_cpp_game/Main.cpp
@@ -1,11 +1,12 @@
 #include "raylib.h"
+#include "raymath.h" // sqrt 계산을 위해 math 라이브러리도 사용합니다.
 
 // 화면에 그릴 수 있는 여러가지는 https://www.raylib.com/cheatsheet/cheatsheet.html 사이트를 참고하세요
 // 빌드 결과 .exe 파일은 "CPP_Practice_Game/bin/Debug 또는 Release 폴더 안에 들어가게 되어 있습니다.
 
 
 // 플레이어의 이동 관련한 연산을 모두 이곳에 모아 놓으면 보기가 좋겠죠?
-void Update(int& playerPositionX, int& playerPositionY)
+void UpdatePlayer(float& playerPositionX, float& playerPositionY)
 {
 	// IsKeyDown(keyCode) 함수를 사용하면, 해당 키보드가 눌렸는지 여부를 bool로 반환해줍니다.
 
@@ -16,8 +17,31 @@ void Update(int& playerPositionX, int& playerPositionY)
 	else if (IsKeyDown(KEY_S)) playerPositionY += 2;
 }
 
+void UpdateEnemy(float enemySpeed, float* enemyPositionX, float* enemyPositionY, int enemyCount, float playerPositionX, float playerPositionY)
+{
+	// playerPositionX & Y는 변하지 않는 값이기 때문에 복사해서 받습니다. const 참조자로 받아도 되지만, 기본 자료형이니 굳이 그럴 필요가 없습니다.
+
+	// 조금 더 흥미로운 내용을 위해서, 적들에게 간단한 이동 로직을 추가해 보겠습니다.
+	// 적들이 Update마다 플레이어의 방향으로 움직이는 로직을 추가하려고 합니다.
+	for (int i = 0; i < enemyCount; i++)
+	{
+		float vectorX = playerPositionX - enemyPositionX[i]; // enemy에서 player로 향하는 벡터의 x방향 성분
+		float vectorY = playerPositionY - enemyPositionY[i]; // enemy에서 player로 향하는 벡터의 y방향 성분
+
+		float length = sqrt(vectorX * vectorX + vectorY * vectorY); // 벡터의 길이
+
+		vectorX = vectorX / length; // 길이가 1인 벡터의 x방향 성분
+		vectorY = vectorY / length; // 길이가 1인 벡터의 y방향 성분
+
+		enemyPositionX[i] += vectorX * enemySpeed; // speed 만큼의 속도로 player쪽으로 이동하게 됩니다
+		enemyPositionY[i] += vectorY * enemySpeed; 
+	}
+	
+
+}
+
 // 적을 그리는 연산은 이 곳에 함수로 모아 뒀습니다.
-void DrawEnemy(int enemyPositionX[], int enemyPositionY[], int enemyCount, int enemyRadius, Color enemyColor)
+void DrawEnemy(float* enemyPositionX, float* enemyPositionY, int enemyCount, float enemyRadius, Color enemyColor)
 {
 	for (int i = 0; i < enemyCount; i++)
 	{
@@ -39,17 +63,19 @@ int main() {
 	SetTargetFPS(60); // 화면을 1초에 몇번 그릴 것인지 (보통은 60번을 사용함. 60 fps(frames per second)
 
 	//--- 화면에 그릴 플레이어 관련 변수
-	int playerPositionX = 10;
-	int playerPositionY = 10;
-	const int playerWidth = 20; // 플레이어 사각형의 크기는 변하지 않는다고 합시다.
-	const int playerHeight = 20;
+	float playerPositionX = 10;
+	float playerPositionY = 10;
+	const float playerWidth = 20; // 플레이어 사각형의 크기는 변하지 않는다고 합시다.
+	const float playerHeight = 20;
 	const Color playerColor = Color{ 238, 108, 77, 255 };
 
 	//--- 화면에 그릴 적 관련 변수
-	const int enemyCount = 10; // 적은 10 마리를 그릴 예정입니다. 배열 크기로 사용하기 위해 const 여야 한다는 것을 잊지 마세요
-	int enemyPositionX[enemyCount]; 
-	int enemyPositionY[enemyCount]; // 적의 X위치와 Y위치를 저장할 배열을 만들어 줍니다.
-	const int enemyRadius = 10; // 적은 동그라미로 그릴 것이라 width/height 대신 반지름이 필요합니다.
+	const float enemyCount = 10; // 적은 10 마리를 그릴 예정입니다. 배열 크기로 사용하기 위해 const 여야 한다는 것을 잊지 마세요
+	// 이번 장에서는 적의 X와 Y위치를 힙 메모리에 저장해 봅시다. 사실 크게 바뀔 건 없죠? 대신, 해제 부분을 절대 잊으시면 안됩니다.
+	float* enemyPositionX = new float[enemyCount];
+	float* enemyPositionY = new float[enemyCount];
+	const float enemySpeed = 1.5f; // enemy는 player보다 약간 느린 속도로 움직입니다.
+	const float enemyRadius = 10; // 적은 동그라미로 그릴 것이라 width/height 대신 반지름이 필요합니다.
 	const Color enemyColor = Color{ 200, 150, 255, 255 };
 
 	// 적의 X, Y 위치 배열에 값을 넣어주겠습니다.
@@ -70,10 +96,13 @@ int main() {
 		// 여기에서도 이렇게 두 부분으로 구분해서 구현할 것이며, 중괄호, 들여쓰기 등은 문법적으로 의미는 없고, 보기 좋게 하기위해 들어간 것입니다.
 
 		{ // 데이터 조작 (Update)
-			
+
 			// 이제 키보드를 사용해 플레이어를 움직일 수 있습니다. Update() 함수에서 이에 대한 처리를 담당합니다.
 			// Update 함수 안에서 playerPositionX, playerPositionY 변수의 값을 바꾸어야 하니, 참조자로 넘겨줍니다.
-			Update(playerPositionX, playerPositionY);
+			UpdatePlayer(playerPositionX, playerPositionY);
+
+			// 이제 적들도 움직입니다.
+			UpdateEnemy(enemySpeed, enemyPositionX, enemyPositionY, enemyCount, playerPositionX, playerPositionY);
 		}
 
 		{ // 그리기 (Draw)
@@ -89,12 +118,16 @@ int main() {
 		}
 	}
 
+	{
+		// 메모리 해제는 절대 잊으시면 안됩니다!
+		delete[] enemyPositionX;
+		delete[] enemyPositionY;
+	}
+
 	return 0;
 }
 
 //--- Practice
 
-// 1. 적들의 숫자를 늘려 보세요.
-// 2. 적들을 화면 위쪽에서 랜덤하게 생성되도록 바꾸어 보세요.
-// 3. 적들이 (자동으로) 왼쪽으로 움직이도록 하려면 어떻게 해야 할까요? 여러가지 방법이 있을 수 있습니다! 
-// 4. 플레이어에게도 테두리를 그려 보세요.
+// 1. 적들의 이동속도를 더 빠르게, 혹은 더 느리게 바꿔 보세요
+// 2. 시간이 지나면 적들이 겹쳐 버립니다. 어떻게 해결할 수 있을까요? 
