77659ed3819e19179f1d0708c97326052f8620a3
Hyungki Kim <diskhkme@gmail.com>
2022-09-04 14:39:40 +0900

    class, vector2 math

--- raylib_cpp_game/Bullet.cpp
+++ raylib_cpp_game/Bullet.cpp
@@ -8,12 +8,8 @@ Bullet::Bullet()
 }
 
 Bullet::Bullet(const Vector2 & position, const Vector2 & direction, const Vector2 & size, const Color & color, float speed)
-	:position{ position }, direction{ direction }, size{ size }, color{ color }, speed{speed}
+	:position{ position }, direction{ Vector2Normalize(direction) }, size{ size }, color{ color }, speed{speed}
 {
-	// 올바른 동작을 위해서는 direction 벡터의 길이가 1이여야 합니다.
-	float length = sqrt(direction.x * direction.x + direction.y * direction.y);
-	this->direction.x /= length;
-	this->direction.y /= length;
 }
 
 void Bullet::Update(float tick)
@@ -29,7 +25,5 @@ void Bullet::Draw() const
 
 void Bullet::Move(float tick)
 {
-	// Bullet은 direction 방향으로 speed 속도로 날아갑니다.
-	position.x += direction.x * speed * tick;
-	position.y += direction.y * speed * tick;
+	position = Vector2Add(position, Vector2Scale(direction, speed*tick));
 }
